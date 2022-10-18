40827635f028339d1f707f596b420c53ad04a64e
salievvm <salievvm@gmail.com>
2022-10-19 00:33:35 +0300

    users_rest_full

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
+import { User } from './users/users.model';
 
 @Module({
     controllers: [
@@ -11,7 +12,7 @@ import { UsersModule } from './users/users.module';
         
     ],
     imports: [
-        ConfigModule.forRoot({
+    ConfigModule.forRoot({
             envFilePath: `.${process.env.NODE_ENV}.env`
         }),
         SequelizeModule.forRoot({
@@ -22,7 +23,7 @@ import { UsersModule } from './users/users.module';
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
             autoLoadModels: true,
-            models: []
+            models: [User]
         }),
         UsersModule
     ],
